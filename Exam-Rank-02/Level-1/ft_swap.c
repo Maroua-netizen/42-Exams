3b6452e6bad86f3c11b80db7f6471c0b5b38db0d
Maroua Mounsif <mmounsif@c1r2p5.1337.ma>
2025-01-28 17:51:19 +0100

    28/01/25

--- Exam-Rank-02/Level-1/ft_swap.c
+++ Exam-Rank-02/Level-1/ft_swap.c
@@ -14,5 +14,22 @@ void	ft_swap(int *a, int *b);
 
 void	ft_swap(int *a, int *b)
 {
-	
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
 }
+
+// #include <stdio.h>
+
+// int main()
+// {
+// 	int a;
+// 	int b;
+
+// 	a = 1;
+// 	b = 2;
+// 	ft_swap(&a, &b);
+// 	printf("a = %d\nb = %d\n", a, b);
+// }
