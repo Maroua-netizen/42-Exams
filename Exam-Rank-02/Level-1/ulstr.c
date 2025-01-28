36e1131ea5a0fe13b65add03bba2734855200151
Maroua Mounsif <mmounsif@c1r2p5.1337.ma>
2025-01-28 20:45:28 +0100

    28/01/25

---
+++ Exam-Rank-02/Level-1/ulstr.c
@@ -0,0 +1,55 @@
+/*
+Assignment name  : ulstr
+Expected files   : ulstr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and reverses the case of all its letters.
+Other characters remain unchanged.
+
+You must display the result followed by a '\n'.
+
+If the number of arguments is not 1, the program displays '\n'.
+
+Examples :
+
+$>./ulstr "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification." | cat -e
+l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$
+$>./ulstr "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  " | cat -e
+s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $
+$>./ulstr "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot" | cat -e
+3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$
+$>./ulstr | cat -e
+$
+*/
+
+#include <unistd.h>
+
+int ft_isalpha(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (2);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while(argv[1][i])
+		{
+			if (ft_isalpha(argv[1][i]) == 1)
+				argv[1][i] -= 32;
+			else if (ft_isalpha(argv[1][i]) == 2)
+				argv[1][i] += 32;
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
