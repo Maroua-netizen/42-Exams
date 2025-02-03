ac81641d203e3a1d9fc33eafbb546729304931a7
Maroua Mounsif <mmounsif@c1r7p1.1337.ma>
2025-02-03 13:26:02 +0100

    Add ft_atoi function implementation

---
+++ Exam-Rank-02/Level-2/ft_atoi.c
@@ -0,0 +1,49 @@
+/*
+Assignment name  : ft_atoi
+Expected files   : ft_atoi.c
+Allowed functions: None
+--------------------------------------------------------------------------------
+
+Write a function that converts the string argument str to an integer (type int)
+and returns it.
+
+It works much like the standard atoi(const char *str) function, see the man.
+
+Your function must be declared as follows:
+
+int	ft_atoi(const char *str);
+*/
+
+int	ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int n;
+
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	sign = 1;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	n = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + str[i] - 48;
+		i++;
+	}
+	return (n * sign);
+}
+
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// int main()
+// {
+// 	printf("%d\n", ft_atoi(" \n  -2147483648"));
+// }
