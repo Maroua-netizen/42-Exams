d967234b9546c012c881680c96f5a4d00dfd4f79
Maroua Mounsif <mmounsif@c1r8p6.1337.ma>
2025-02-26 08:50:32 +0000

    Update split function

--- Exam-Rank-02/Level-4/ft_split.c
+++ Exam-Rank-02/Level-4/ft_split.c
@@ -17,99 +17,69 @@ char    **ft_split(char *str);
 
 #include <stdlib.h>
 
-int		wordcount(char *str);
-char	*get_next_word(char *str);
-
-char    **ft_split(char *str)
+int	wordcount(char *str)
 {
-	char	**strs;
-	int		len = wordcount(str);
-
-	strs = malloc((len + 1) * sizeof(char *));
-	if (!strs)
-		return (NULL);
-	strs[len] = NULL;
-	int	i = 0;
-	while (i < len)
-	{
-		strs[i] = get_next_word(str);
-		if (!strs[i])
-		{
-			i = 0;
-			while (strs[i])
-			{
-				free(strs[i]);
-				i++;
-			}
-			free(strs);
-			return (NULL);
-		}
-		i++;
-	}
-	return (strs);
-}
-
-char	*get_next_word(char *str)
-{
-	static int	i = 0;
-	char		*next_str;
-
-	while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
-		i++;
-	int	len = 0;
-	while (str[i] && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
-	{
-		len++;
-		i++;
-	}
-	int	last_i = i;
-	next_str = malloc(len + 1);
-	if (!next_str)
-		return (NULL);
-	next_str[len] = '\0';
-	while (len - 1 >= 0)
-	{
-		next_str[len - 1] = str[last_i - 1];
-		last_i--;
-		len--;
-	}
-	return (next_str);
-}
-
-int		wordcount(char *str)
-{
-	int	count = 0;
-	int	i = 0;
-
+	int count = 0;
+	int i = 0;
 	while (str[i])
 	{
-		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' 
-			&& (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\n' || !str[i + 1]))
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' &&
+		(str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\n' || !str[i + 1]))
 			count++;
 		i++;
 	}
 	return (count);
 }
 
+char	*get_next_word(char *str)
+{
+	char	*next_word;
+	static int	i = 0;
+	while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+		i++;
+	int j = i;
+	while (str[j] && str[j] != ' ' && str[j] != '\t' && str[j] != '\n')
+		j++;
+	int len = j - i;
+	next_word = malloc(len + 1);
+	if (!next_word)
+		return (NULL);
+	next_word[len] = '\0';
+	int k = 0;
+	while (i < j)
+	{
+		next_word[k] = str[i];
+		i++;
+		k++;
+	}
+	return (next_word);
+}
+
+char    **ft_split(char *str)
+{
+	char	**splited = malloc((wordcount(str) + 1) * sizeof(char *));
+	if (!splited)
+		return (NULL);
+	splited[wordcount(str)] = NULL;
+	int i = 0;
+	while (i < wordcount(str))
+	{
+		splited[i] = get_next_word(str);
+		i++;
+	}
+	return (splited);
+}
+
 // #include <stdio.h>
 
-// void	leaks()
+// int main()
 // {
-// 	system("leaks -q a.out");
-// }
-
-// int		main()
-// {
-// 	atexit(leaks);
-// 	char	str[] = " Hello world!";
-
-// 	char	**strs = ft_split(str);
-// 	int	i = 0;
-// 	while (strs[i])
+// 	char str[] = "  this is a \nsplit test !\t";
+// 	char **splited = ft_split(str);
+// 	int i = 0;
+// 	while (splited[i])
 // 	{
-// 		printf("%s\n", strs[i]);
-// 		free(strs[i]);
+// 		printf("%s\n", splited[i]);
 // 		i++;
 // 	}
-// 	free(strs);
 // }
